049059342b96fa791cec56c6d708ccb3d2693402
dlmather <david@nodeprime.com>
2014-07-17 10:48:48 -0700

    Minor changes

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ var reg = require('./lib/').Registry;
 var logger = console;
 var registry = new reg();
 registry.connect();
-registry.announce('schopanhauer', 'gosamer', 'scooby-snax', function (err, res) {
+registry.announce('schoepanhauer', 'gosamer', 'scooby-snax', function (err, res) {
   console.log(err, res);
-  registry.resolve('schopanhauer', 'gosamer', function (err, domains) {
+  registry.resolve('schoepanhauer', 'gosamer', function (err, domains) {
     if(err) {
       logger.error(err);
     } else {
