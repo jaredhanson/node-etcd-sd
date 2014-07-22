fcaa24a8da21198204e7f76a6ae0307c3394b84c
dlmather <david@nodeprime.com>
2014-07-22 11:56:08 -0700

    Added timeout/ttl for nodes to maintain services being up

--- test.js
+++ test.js
@@ -2,9 +2,8 @@ var reg = require('./lib/').Registry;
 var logger = console;
 var registry = new reg();
 registry.connect();
-registry.announce('schoepanhauer', 'gosamer', 'scooby-snax', function (err, res) {
-  console.log(err, res);
-  registry.resolve('schoepanhauer', 'gosamer', function (err, domains) {
+registry.announce('mr', 'ttl', 'is', function (err, res) {
+  registry.resolve('mr', 'ttl', function (err, domains) {
     if(err) {
       logger.error(err);
     } else {
