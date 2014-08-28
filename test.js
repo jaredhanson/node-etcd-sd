a6712334332d82aecc38af0a0a3c257191cddc62
dlmather <david@nodeprime.com>
2014-08-27 17:12:40 -0700

    Error with ttl now emits a ttl, not a generic error

--- test.js
+++ test.js
@@ -3,20 +3,29 @@ var logger = console;
 var registry = new reg();
 var count = 0;
 registry.connect();
-registry.announce('mr', 'ttl', 'is', function (err, res) {
-  if (err) { logger.log (err); }
-  registry.resolve('mr', 'ttl', function (err, value) {
-    if (err) {
-      logger.error(err);
-    } else {
-      logger.log(value);
-    }
+var uid1 = registry.announce('mr', 'ttl', 'is', function (err, res) {
+  var uid2 = registry.announce('mr', 'ttl', 'isnot', function (err, res) {
+    registry.resolve('mr', 'ttl', function (err, value) {
+      if (err) {
+        logger.error(err);
+      } else {
+        logger.log(value);
+      }
+      console.log(uid1, uid2);
+      registry.unannounce('mr', 'ttl', uid1, function (err, res) {
+        if (err) { logger.error(err); }
+        registry.resolve('mr', 'ttl', function (err, value) {
+          if (err) {
+            logger.error(err);
+          } else {
+            logger.log(value);
+          }
+        });
+      });
+    });
   });
 });
 
-registry.on('ttl-refresh', function () {
-  count += 1;
-  if (count === 1) {
-    registry.close();
-  }
+registry.on('error', function (err) {
+  registry.close();
 });
